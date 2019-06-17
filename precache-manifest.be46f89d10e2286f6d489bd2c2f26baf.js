a40a1478abd62a3a09941c40d8bb7035ff088fbe
Ashun Kothari <ashun.kothari10@gmail.com>
2019-06-18 02:31:13 +0530

    Updates

---
+++ precache-manifest.be46f89d10e2286f6d489bd2c2f26baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6653b3cd3e2aeae8a488b8c79ce6cbba",
+    "url": "/react-movie-db/index.html"
+  },
+  {
+    "revision": "f8577a025455fd0b4bdd",
+    "url": "/react-movie-db/static/css/main.03591acf.chunk.css"
+  },
+  {
+    "revision": "3dc6f1d4de23f2cc1f20",
+    "url": "/react-movie-db/static/js/2.6cf689d6.chunk.js"
+  },
+  {
+    "revision": "f8577a025455fd0b4bdd",
+    "url": "/react-movie-db/static/js/main.067d1909.chunk.js"
+  },
+  {
+    "revision": "821738a708f18df07a9f",
+    "url": "/react-movie-db/static/js/runtime~main.439a8ae0.js"
+  }
+]);
